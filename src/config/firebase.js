0cfe4b8d301d2868140f133ece76c187ef56b953
alvindivina08 <alvindivina08@gmail.com>
2024-03-30 19:31:32 -0500

    implement .env file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,19 +4,18 @@ import { getFirestore, collection } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDkERNuXGi3L1pwH-W-Bkk8YuJLQrNMUaA",
-  authDomain: "alvin-project-b084f-102c8.firebaseapp.com",
-  projectId: "alvin-project-b084f",
-  storageBucket: "alvin-project-b084f.appspot.com",
-  messagingSenderId: "880277417860",
-  appId: "1:880277417860:web:8f7697b95bddfd572337d1",
-  measurementId: "G-352L46KK0J",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-
 export const db = getFirestore(app);
 export const storage = getStorage(app);
